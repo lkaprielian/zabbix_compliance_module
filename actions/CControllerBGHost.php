9d8d02d07973acc191c8224b477066cc88d94a0c
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-07 15:09:05 +0100

    Update CControllerBGHost.php

--- actions/CControllerBGHost.php
+++ actions/CControllerBGHost.php
@@ -205,15 +205,15 @@ abstract class CControllerBGHost extends CController {
 
 		// print_r($hosts);
 		foreach ($hosts as &$host){
-			foreach ($host['hostgroups'] as $group_tt) {
-				// $groupid = $group['groupid'];
-				$groupname_tt = $group_tt['name'];
-				// if (str_contains($groupname_full, '/')) {
-				// 	print_r($groupname_full);
-				// }
-				print_r($groupname_tt);
+			foreach ($host['hostgroups'] as $group) {
+				$groupid = $group['groupid'];
+				$groupname_full = $group['name'];
+				if (str_contains($groupname_full, '/')) {
+					print($groupname_full);
+				}
 			// unset($tthosts[0]);
 			// print_r($tthosts );
+			}
 		}
 		foreach ($hosts as &$host) {
 			// unset($host['hostgroups'][0]);
