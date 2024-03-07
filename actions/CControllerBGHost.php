fb6633a19bce550d11f08a9e8b435b07f0f5b518
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-07 17:21:42 +0100

    Update CControllerBGHost.php

--- actions/CControllerBGHost.php
+++ actions/CControllerBGHost.php
@@ -332,10 +332,10 @@ abstract class CControllerBGHost extends CController {
 		// unset($group);
 
 		// Remove groups that are not to be shown from 'children' groups list
-		print_r($host_groups_to_show);
+		// print_r($host_groups_to_show);
 		foreach ($host_groups_to_show as $group_name) {
 			print_r($group);
-			}
+		}
 			// $groups_to_delete = [];
 			// print_r($group_name['parent_group_name']);
 		// }
