c3c430545286c3ac149dd90e1cccc23523f6f04c
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-07 15:36:11 +0100

    Update CControllerBGHost.php

--- actions/CControllerBGHost.php
+++ actions/CControllerBGHost.php
@@ -287,10 +287,11 @@ abstract class CControllerBGHost extends CController {
 					$host_groups_to_show[$group['name']]['hosts'] = [ $host['hostid'] ];
 					// Make sure parent group exists as well
 					$grp_arr = explode('/', $group['name']);
-					print_r($grp_arr);
+					// print_r($grp_arr);
 					for ($i = 1, $g_name = $grp_arr[0]; $i < count($grp_arr); $i++) {
 						if (!array_key_exists($g_name, $host_groups_to_show)) {
 							$host_groups_to_show[$g_name] = $host_groups[$g_name];
+							print_r($host_groups_to_show[$g_name]);
 							$host_groups_to_show[$g_name]['hosts'] = [];
 						}
 						$g_name = $g_name.'/'.$grp_arr[$i];
