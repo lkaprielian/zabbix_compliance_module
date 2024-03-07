5a4d47d5ee475dd0741b7f16695cbd8ed772032b
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-07 11:46:31 +0100

    Update CControllerBGHost.php

--- actions/CControllerBGHost.php
+++ actions/CControllerBGHost.php
@@ -171,7 +171,8 @@ abstract class CControllerBGHost extends CController {
 			'tags' => $filter['tags'],
 			'inheritedTags' => true,
 			// 'groupids' => $filter['groupids']  ? $filter['groupids']  : null,
-			'groupids' => $groupids,
+			'groupids' => [['mag2']],
+			// 'groupids' => $groupids,
 			'severities' => $filter['severities'] ? $filter['severities'] : null,
 			'withProblemsSuppressed' => $filter['severities']
 				? (($filter['show_suppressed'] == ZBX_PROBLEM_SUPPRESSED_TRUE) ? null : false)
@@ -202,34 +203,32 @@ abstract class CControllerBGHost extends CController {
 
 		foreach ($hosts as &$host) {
 			foreach ($host['hostgroups'] as $group) {
-				if ($group != "all_ap") {
-					$groupid = $group['groupid'];
-					$groupname_full = $group['name'];
-					if (!array_key_exists($groupname_full, $host_groups)) {
-						$host_groups[$groupname_full] = [
-							'groupid' => $groupid,
-							'hosts' => [
-								$host['hostid']
-							],
-							'children' => [],
-							'parent_group_name' => '',
-							'num_of_hosts' => 1,
-							'problem_count' => [],
-							'is_collapsed' => true
-						];
-						for ($severity = TRIGGER_SEVERITY_COUNT - 1; $severity >= TRIGGER_SEVERITY_NOT_CLASSIFIED; $severity--) {
-							$host_groups[$groupname_full]['problem_count'][$severity] = 0;
-						}
-					} else {
-						$host_groups[$groupname_full]['hosts'][] = $host['hostid'];
-						$host_groups[$groupname_full]['num_of_hosts']++;
+				$groupid = $group['groupid'];
+				$groupname_full = $group['name'];
+				if (!array_key_exists($groupname_full, $host_groups)) {
+					$host_groups[$groupname_full] = [
+						'groupid' => $groupid,
+						'hosts' => [
+							$host['hostid']
+						],
+						'children' => [],
+						'parent_group_name' => '',
+						'num_of_hosts' => 1,
+						'problem_count' => [],
+						'is_collapsed' => true
+					];
+					for ($severity = TRIGGER_SEVERITY_COUNT - 1; $severity >= TRIGGER_SEVERITY_NOT_CLASSIFIED; $severity--) {
+						$host_groups[$groupname_full]['problem_count'][$severity] = 0;
 					}
+				} else {
+					$host_groups[$groupname_full]['hosts'][] = $host['hostid'];
+					$host_groups[$groupname_full]['num_of_hosts']++;
+				}
 
-					$grp_arr = explode('/', $groupname_full);
-					if (count($grp_arr) > 1) {
-						// Find all parent groups and create respective array elements in $host_groups
-						$this->add_parent($host_groups, $fake_group_id, $groupname_full, $filter);
-					}
+				$grp_arr = explode('/', $groupname_full);
+				if (count($grp_arr) > 1) {
+					// Find all parent groups and create respective array elements in $host_groups
+					$this->add_parent($host_groups, $fake_group_id, $groupname_full, $filter);
 				}
 			}
 		}
