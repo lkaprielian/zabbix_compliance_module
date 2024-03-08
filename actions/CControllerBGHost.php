14f63fe7d6160c1280b5f69fb7c2d9b49af2ccc7
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-08 15:53:41 +0100

    Update CControllerBGHost.php

--- actions/CControllerBGHost.php
+++ actions/CControllerBGHost.php
@@ -283,23 +283,27 @@ abstract class CControllerBGHost extends CController {
 		// print_r($hosts_sorted_by_group);
 		foreach ($hosts_sorted_by_group as $host) {
 			if (count($host['hostgroups']) == 2) {
-				print_r($host['hostgroups']);
-				foreach ($host['hostgroups'] as $group) {
-					if (!array_key_exists($group['name'], $host_groups_to_show)) {					
-						$host_groups_to_show[$group['name']] = $host_groups[$group['name']];
-						$host_groups_to_show[$group['name']]['hosts'] = [ $host['hostid'] ];
-						// Make sure parent group exists as well
-						$grp_arr = explode('/', $group['name']);
-						// print_r($grp_arr);
-						for ($i = 1, $g_name = $grp_arr[0]; $i < count($grp_arr); $i++) {
-							if (!array_key_exists($g_name, $host_groups_to_show)) {
-								$host_groups_to_show[$g_name] = $host_groups[$g_name];
-								$host_groups_to_show[$g_name]['hosts'] = [];
+				// print_r($host['hostgroups']);
+				$tst_subgroup =  $host['hostgroups'];
+				for ($x=1, $subgroup = $tst_subgroup[0]; $x < count($tst_subgroup); $x++) {
+					print($subgroup);
+					foreach ($host['hostgroups'] as $group) {
+						if (!array_key_exists($group['name'], $host_groups_to_show)) {					
+							$host_groups_to_show[$group['name']] = $host_groups[$group['name']];
+							$host_groups_to_show[$group['name']]['hosts'] = [ $host['hostid'] ];
+							// Make sure parent group exists as well
+							$grp_arr = explode('/', $group['name']);
+							// print_r($grp_arr);
+							for ($i = 1, $g_name = $grp_arr[0]; $i < count($grp_arr); $i++) {
+								if (!array_key_exists($g_name, $host_groups_to_show)) {
+									$host_groups_to_show[$g_name] = $host_groups[$g_name];
+									$host_groups_to_show[$g_name]['hosts'] = [];
+								}
+								$g_name = $g_name.'/'.$grp_arr[$i];
 							}
-							$g_name = $g_name.'/'.$grp_arr[$i];
+						} else {
+							$host_groups_to_show[$group['name']]['hosts'][] = $host['hostid'];
 						}
-					} else {
-						$host_groups_to_show[$group['name']]['hosts'][] = $host['hostid'];
 					}
 				}
 				// if (str_contains($group['name'], '/')) {
