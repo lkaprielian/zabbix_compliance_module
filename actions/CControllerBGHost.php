e4ce345a1bb358f497c2422060c7e1d48579ba3c
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-08 15:36:17 +0100

    Update CControllerBGHost.php

--- actions/CControllerBGHost.php
+++ actions/CControllerBGHost.php
@@ -283,23 +283,26 @@ abstract class CControllerBGHost extends CController {
 		print_r($hosts_sorted_by_group);
 		foreach ($hosts_sorted_by_group as $host) {
 			foreach ($host['hostgroups'] as $group) {
-				// if (str_contains($group['name'], '/')) {
-				if (!array_key_exists($group['name'], $host_groups_to_show)) {					
-					$host_groups_to_show[$group['name']] = $host_groups[$group['name']];
-					$host_groups_to_show[$group['name']]['hosts'] = [ $host['hostid'] ];
-					// Make sure parent group exists as well
-					$grp_arr = explode('/', $group['name']);
-					// print_r($grp_arr);
-					for ($i = 1, $g_name = $grp_arr[0]; $i < count($grp_arr); $i++) {
-						if (!array_key_exists($g_name, $host_groups_to_show)) {
-							$host_groups_to_show[$g_name] = $host_groups[$g_name];
-							$host_groups_to_show[$g_name]['hosts'] = [];
+				if (count($group) > 1) {
+					print_r($group);
+					if (!array_key_exists($group['name'], $host_groups_to_show)) {					
+						$host_groups_to_show[$group['name']] = $host_groups[$group['name']];
+						$host_groups_to_show[$group['name']]['hosts'] = [ $host['hostid'] ];
+						// Make sure parent group exists as well
+						$grp_arr = explode('/', $group['name']);
+						// print_r($grp_arr);
+						for ($i = 1, $g_name = $grp_arr[0]; $i < count($grp_arr); $i++) {
+							if (!array_key_exists($g_name, $host_groups_to_show)) {
+								$host_groups_to_show[$g_name] = $host_groups[$g_name];
+								$host_groups_to_show[$g_name]['hosts'] = [];
+							}
+							$g_name = $g_name.'/'.$grp_arr[$i];
 						}
-						$g_name = $g_name.'/'.$grp_arr[$i];
+					} else {
+						$host_groups_to_show[$group['name']]['hosts'][] = $host['hostid'];
 					}
-				} else {
-					$host_groups_to_show[$group['name']]['hosts'][] = $host['hostid'];
 				}
+				// if (str_contains($group['name'], '/')) {
 			}
 		}
 
