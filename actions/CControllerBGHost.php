92dcfb2057701c7cfc5f85f326ee9f294b1e051c
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-08 22:03:46 +0100

    Update CControllerBGHost.php

--- actions/CControllerBGHost.php
+++ actions/CControllerBGHost.php
@@ -429,7 +429,7 @@ abstract class CControllerBGHost extends CController {
 					$hosts = array_merge($hosts, $group['hosts']);
 				}
 			}
-			print_r($group);
+			print_r($hosts);
 			foreach ($group['children'] as $child_group_name) {
 				if (!array_key_exists($child_group_name, $host_groups_to_show)) {
 					$groups_to_delete[] = $child_group_name;
