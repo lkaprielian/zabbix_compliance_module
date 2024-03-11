2d0c75ab98bdc51a530788bf76cebc771f4043b5
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-11 01:21:31 +0100

    Update CControllerBGHost.php

--- actions/CControllerBGHost.php
+++ actions/CControllerBGHost.php
@@ -468,11 +468,11 @@ abstract class CControllerBGHost extends CController {
 		foreach ($host_groups_to_show as $groupName => $group) {
 			print_r(array_intersect($duplicateHosts, $group['hosts']));
 			// Check if hosts and parent_group_name meet deletion conditions
-			// if (array_intersect($duplicateHosts, $group['hosts']) === $duplicateHosts && empty($group['parent_group_name'])) {
-			// 	$groupsToDelete[] = $groupName;
-			// }
+			if (array_intersect($duplicateHosts, $group['hosts']) && empty($group['parent_group_name'])) {
+				$groupsToDelete[] = $groupName;
+			}
 		}
-		// print_r($groupsToDelete);
+		print_r($groupsToDelete);
 		// Remove groups that meet the deletion conditions
 		// foreach ($groupsToDelete as $groupName) {
 		// 	unset($host_groups_to_show[$groupName]);
