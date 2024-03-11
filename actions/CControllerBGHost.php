d364d3d72dde04b0cd646c6bf1fb1519ff6b85bf
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-11 01:12:20 +0100

    Update CControllerBGHost.php

--- actions/CControllerBGHost.php
+++ actions/CControllerBGHost.php
@@ -461,11 +461,11 @@ abstract class CControllerBGHost extends CController {
 
 		// Identify duplicate hosts
 		$duplicateHosts = array_diff_assoc($seenHosts, array_unique($seenHosts));
-		// print_r($duplicateHosts);
+		print_r($duplicateHosts);
 
 		foreach ($host_groups_to_show as $group_name => &$group) {
 			foreach ($duplicateHosts as $duphost) {
-				print($duphost);
+				// print($duphost);	
 				// if (empty($group['parent_group_name']) && $duphost == $group['hosts']) {
 				// 	print($duphost);
 				// }
