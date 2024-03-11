f24065c9af66a37ff58f6b1a25de88e02988bf60
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-11 01:18:25 +0100

    Update CControllerBGHost.php

--- actions/CControllerBGHost.php
+++ actions/CControllerBGHost.php
@@ -471,14 +471,14 @@ abstract class CControllerBGHost extends CController {
 				$groupsToDelete[] = $groupName;
 			}
 		}
-		
+		print_r($groupsToDelete);
 		// Remove groups that meet the deletion conditions
 		foreach ($groupsToDelete as $groupName) {
 			unset($host_groups_to_show[$groupName]);
 		}
 		
 		print_r($host_groups_to_show);
-		
+
 		unset($group);
 		// print_r($groupsToDelete);
 		// // Remove groups with duplicated hostid and empty parent_group_name
